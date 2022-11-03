9c5a61b0746e50d6c6d062c12c3b127e7d2822de
EarthDragon10 <lagudadev@gmail.com>
2022-11-03 16:23:24 +0100

    add logic for take contact details

---
+++ my-app/src/app/classes/rubric.ts
@@ -0,0 +1,5 @@
+export class Rubric {
+  name?: string;
+  lastname?: string;
+  address?: string;
+}
--- my-app/src/app/main/form-contatti/form-contatti.component.ts
+++ my-app/src/app/main/form-contatti/form-contatti.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Rubric } from 'src/app/classes/rubric';
 
 @Component({
   selector: 'app-form-contatti',
   templateUrl: './form-contatti.component.html',
-  styleUrls: ['./form-contatti.component.css']
+  styleUrls: ['./form-contatti.component.css'],
 })
 export class FormContattiComponent implements OnInit {
+  contact = new Rubric();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  showContact(): void {
+    console.log(this.contact.name, this.contact.lastname, this.contact.address);
   }
-
 }
